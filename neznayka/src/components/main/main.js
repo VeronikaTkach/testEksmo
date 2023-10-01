a48e868b5f675ecfa51a032b25258f357ee310f6
Veronika <xafling@mail.ru>
2023-10-02 00:57:47 +0300

    buttons

--- neznayka/src/components/main/main.js
+++ neznayka/src/components/main/main.js
@@ -1,11 +1,32 @@
 import img1 from '../../img/neznayka.png';
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import './main.css';
-import img2 from "../../img/Group 1321317225.png";
+import microphoneOff from "../../img/microphoneOff.png";
+import microphoneOn from "../../img/microphoneOn.png";
+import buttonOk from "../../img/buttonOk.png";
+
 export const Main = () => {
-    const changeButtonState = (e) => {
-        // e.target.closest('.play_btn').classList.toggle('play_btn--toggle');
+
+    const [btn_mic_on_enabled, setBtn_mic_on_enabled] = useState(true);
+    const [btn_mic_off_enabled, setBtn_mic_off_enabled] = useState(false);
+    const [btn_ok_enabled, setBtn_ok_enabled] = useState(false);
+
+    const changeButtonState = () => {
+        if (btn_mic_on_enabled) {
+            setBtn_mic_on_enabled(false);
+            setBtn_mic_off_enabled(true);
+            setBtn_ok_enabled(false);
+        } else if (btn_mic_off_enabled) {
+            setBtn_mic_on_enabled(false);
+            setBtn_mic_off_enabled(false);
+            setBtn_ok_enabled(true);
+        } else if (btn_ok_enabled) {
+            setBtn_mic_on_enabled(true);
+            setBtn_mic_off_enabled(false);
+            setBtn_ok_enabled(false);
+        }
     }
+
     return (
         <Fragment>
             <div className={'main'}>
@@ -15,9 +36,15 @@ export const Main = () => {
                 <img src={img1} alt="image" className="main_img" />
                 {/*поле ввода текстом*/}
                 <input className={'text_input'} placeholder={'| ввести текстом'}/>
-                {/*кнопки on, off, ok*/}
-                <button type={'button'} className={'button_img'} onClick={changeButtonState}>
-                    <img src={img2} className={'button_img_off'} alt={'image microphone off'}/>
+                {/*кнопки off, on, ok*/}
+                <button type={'button'} id={'button'} className={btn_mic_on_enabled?'button_visible':'button_invisible'} onClick={changeButtonState}>
+                    <img src={microphoneOff} className={''} alt={'image microphone off'}/>
+                </button>
+                <button type={'button'} id={'button'} className={btn_mic_off_enabled?'button_visible':'button_invisible'} onClick={changeButtonState}>
+                    <img src={microphoneOn} className={''} alt={'image microphone on'}/>
+                </button>
+                <button type={'button'} id={'button'} className={btn_ok_enabled?'button_visible':'button_invisible'} onClick={changeButtonState}>
+                    <img src={buttonOk} className={''} alt={'image button ok'}/>
                 </button>
             </div>
         </Fragment>
