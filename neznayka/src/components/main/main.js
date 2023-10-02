dbdee510081c190bef41a77ec5b021548378d3ae
Veronika <xafling@mail.ru>
2023-10-02 22:56:22 +0300

    adaptive layout

--- neznayka/src/components/main/main.js
+++ neznayka/src/components/main/main.js
@@ -15,22 +15,6 @@ export const Main = () => {
     const [show_soundWave, setShow_soundWave] = useState(false);
     const [btn_send, setBtn_send] = useState(false);
 
-    // const showSoundWave
-    //
-    // const showTextINput
-    //
-    // const showBtnOn
-    // const showBtnOff
-    // const showBtnOk
-
-
-
-    // const mic_on_pressed_handler = () => {
-    //     showSoundWave();
-    //     showMicOffButton();
-    //
-    //
-    // }
 
     const mic_off_pressed_handler = () => {
         setBtn_mic_off_enabled(false);
@@ -77,22 +61,6 @@ export const Main = () => {
         setBtn_send(false);
     }
 
-    // const changeButtonState = () => {
-    //     if (btn_mic_on_enabled) {
-    //         setBtn_mic_on_enabled(false);
-    //         setBtn_mic_off_enabled(true);
-    //         setBtn_ok_enabled(false);
-    //     } else if (btn_mic_off_enabled) {
-    //         setBtn_mic_on_enabled(false);
-    //         setBtn_mic_off_enabled(false);
-    //         setBtn_ok_enabled(true);
-    //     } else if (btn_ok_enabled) {
-    //         setBtn_mic_on_enabled(true);
-    //         setBtn_mic_off_enabled(false);
-    //         setBtn_ok_enabled(false);
-    //     }
-    // }
-
     return (
         <Fragment>
             <div className={'main'}>
@@ -114,7 +82,7 @@ export const Main = () => {
                 <button type={'button'} id={'button'} className={btn_ok_enabled?'button_visible':'button_invisible'} onClick={ok_pressed_handler}>
                     <img src={buttonOk} className={'btn_ok'} alt={'image button ok'}/>
                 </button>
-                <button type={'button'} className={btn_send?'button_visible':'button_invisible'} onClick={send_pressed_handler}> Отправить</button>
+                <button type={'button'} id={'button_send'} className={btn_send?'button_visible':'button_invisible'} onClick={send_pressed_handler}> Отправить</button>
             </div>
         </Fragment>
     )
