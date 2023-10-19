1c809c65b7f5420ef54c1da6f0306432e7d90704
Veronika <xafling@mail.ru>
2023-10-19 23:41:20 +0300

    fixed voice recognition

--- neznayka/src/components/main/main.js
+++ neznayka/src/components/main/main.js
@@ -1,6 +1,6 @@
 import img1 from '../../img/neznayka.png';
 import wave from '../../img/wave.png';
-import {Fragment, useState} from "react";
+import {Fragment, useState, useRef} from "react";
 import './main.css';
 import microphoneOff from "../../img/microphoneOff.png";
 import microphoneOn from "../../img/microphoneOn.png";
@@ -16,54 +16,38 @@ export const Main = () => {
     recognition.interimResults = false;
     recognition.maxAlternatives = 1;
 
-    const [btn_mic_on_enabled, setBtn_mic_on_enabled] = useState(true);
-    const [btn_mic_off_enabled, setBtn_mic_off_enabled] = useState(false);
+    const [btn_mic_on_enabled, setBtn_mic_on_enabled] = useState(false);
+    const [btn_mic_off_enabled, setBtn_mic_off_enabled] = useState(true);
     const [btn_ok_enabled, setBtn_ok_enabled] = useState(false);
     const [show_text_input, setShow_text_input] = useState(true);
     const [show_soundWave, setShow_soundWave] = useState(false);
     const [btn_send, setBtn_send] = useState(false);
     const [AI_text, setAI_text] = useState('');
 
-    function setInitialState(){
+    const inputRef = useRef();
 
+    function setInitialState(){
+        setBtn_mic_off_enabled(true);
+        setBtn_mic_on_enabled(false);
+        setBtn_ok_enabled(false);
+        setShow_soundWave(false);
+        setShow_text_input(true);
+        setBtn_send(false);
+        setAI_text('');
+        inputRef.current.value = '';
     }
 
     function setRecordingState(){
-
-    }
-
-    function setTextInputState(){
-
-    }
-
-    function setResultsState(){
-
-    }
-
-
-
-
-    const mic_off_pressed_handler = () => {
         setBtn_mic_off_enabled(false);
         setBtn_mic_on_enabled(true);
         setBtn_ok_enabled(false);
         setShow_soundWave(true);
         setShow_text_input(false);
         setBtn_send(false);
+        recognition.start();
     }
 
-    const mic_on_pressed_handler = () => {
-        setBtn_mic_off_enabled(false);
-        setBtn_mic_on_enabled(false);
-        setBtn_ok_enabled(true);
-        setShow_soundWave(false);
-        setShow_text_input(false);
-        setBtn_send(false);
-
-        // recognition.start();
-    }
-
-    const text_input_handler = () => {
+    function setTextInputState(){
         setBtn_mic_off_enabled(false);
         setBtn_mic_on_enabled(false);
         setBtn_ok_enabled(false);
@@ -72,16 +56,7 @@ export const Main = () => {
         setBtn_send(true);
     }
 
-    const ok_pressed_handler = () => {
-        setBtn_mic_off_enabled(true);
-        setBtn_mic_on_enabled(false);
-        setBtn_ok_enabled(false);
-        setShow_soundWave(false);
-        setShow_text_input(true);
-        setBtn_send(false);
-    }
-
-    const send_pressed_handler = () => {
+    function setResultsState(){
         setBtn_mic_off_enabled(false);
         setBtn_mic_on_enabled(false);
         setBtn_ok_enabled(true);
@@ -90,10 +65,35 @@ export const Main = () => {
         setBtn_send(false);
     }
 
+    const mic_off_pressed_handler = () => {
+        setRecordingState();
+    }
+
+    const mic_on_pressed_handler = () => {
+        recognition.abort();
+        setResultsState();
+    }
+
+    const text_input_handler = () => {
+        setTextInputState();
+    }
+
+    const ok_pressed_handler = () => {
+        setInitialState();
+    }
+
+    const send_pressed_handler = () => {
+        const manual_text = inputRef.current.value;
+
+        setResultsState();
+
+        setAI_text(manual_text);
+    }
+
     recognition.onresult = function (r) {
         const recognizedText = r.results[0][0].transcript;
 
-        mic_off_pressed_handler();
+        setResultsState();
 
         setAI_text(recognizedText);
     }
@@ -106,7 +106,7 @@ export const Main = () => {
                 {/*изображение Незнайки*/}
                 <img src={img1} alt="image" className="main_img" />
                 {/*поле ввода текстом*/}
-                <input id={'text_input'} className={show_text_input?'button_visible':'button_invisible'} placeholder={'| ввести текстом'} onClick={text_input_handler}/>
+                <input id={'text_input'} ref={inputRef} className={show_text_input?'button_visible':'button_invisible'} placeholder={'| ввести текстом'} onClick={text_input_handler}/>
                 {/*картинка волна*/}
                 <img src={wave} alt="image sound wave" id={'soundWave'} className={show_soundWave?'button_visible':'button_invisible'}/>
                 {/*кнопки off, on, ok, Отправить*/}
